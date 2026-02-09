d698a075e11da562de6c5faf658755b8b8b705b8
Darachhat <darachhat012@gmail.com>
2026-02-09 11:54:10 +0700

    Add

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
         Schedule::command('payout:vendors')
             ->monthlyOn(1, '00:00')
             ->withoutOverlapping();
